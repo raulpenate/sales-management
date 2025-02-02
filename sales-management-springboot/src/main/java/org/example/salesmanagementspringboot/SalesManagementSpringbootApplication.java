9c6bb9f839b03664df710f250c2dc6ab1634344b
raulpenate <50472064+raulpenate@users.noreply.github.com>
2025-02-02 15:34:24 -0600

    🎉 Creating springboot project

---
+++ sales-management-springboot/src/main/java/org/example/salesmanagementspringboot/SalesManagementSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.example.salesmanagementspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesManagementSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesManagementSpringbootApplication.class, args);
+    }
+
+}
